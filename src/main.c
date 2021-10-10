4c9cf7264f81772d24f6bb1025b49da255343c6b
OSapy <oscarsapy@gmail.com>
2021-10-10 17:24:30 +0200

    final commit

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/**
+/*
  * @file main.c
  * @authors Pierre JANVIER et Oscar SAPY
  * @brief Ce programme permet de confronter et de déterminer le meilleur algortihme de tri par rapport à un tableau de X valeurs allant de 0 à 10⁶
@@ -6,7 +6,7 @@
  * @date 2021-10-10
  * @copyright Copyright (c) 2021
  * 
- */
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -15,183 +15,183 @@
 #include "../include/tri_selection.h"
 #include "../include/tri_tas.h" */
 
-/**
+/*
  * @brief Cette fonction permet la création d'un fichier .csv
  * @param filename 
  * @param csvtab 
- */
+*/
 void create_csv(char *filename, float csvtab[5][7])
 {
 
-	FILE *fp;
-	printf("%s", filename);
-	fp = fopen(filename, "+w");
+    FILE *fp;
+    printf("%s", filename);
+    fp = fopen(filename, "+w");
 
-	//Définition du model du fichier .csv qui va être généré
-	fprintf(fp, "Taille tableau / tri", "Tri à bulle", "Tri par sélection", "Tri par insertion", "Tri par tas");
-	fprintf(fp, "100", csvtab[0][0], csvtab[0][1], csvtab[0][2], csvtab[0][3]);
-	fprintf(fp, "10³", csvtab[1][0], csvtab[1][1], csvtab[1][2], csvtab[1][3]);
-	fprintf(fp, "10⁴", csvtab[2][0], csvtab[2][1], csvtab[2][2], csvtab[2][3]);
-	fprintf(fp, "10⁵", csvtab[3][0], csvtab[3][1], csvtab[3][2], csvtab[3][3]);
-	fprintf(fp, "10⁶", csvtab[4][0], csvtab[4][1], csvtab[4][2], csvtab[4][3]);
-	fprintf(fp, "10⁷", csvtab[5][0], csvtab[5][1], csvtab[5][2], csvtab[5][3]);
+    //Définition du model du fichier .csv qui va être généré
+    fprintf(fp, "Taille tableau / tri", "Tri à bulle", "Tri par sélection", "Tri par insertion", "Tri par tas");
+    fprintf(fp, "100", csvtab[0][0], csvtab[0][1], csvtab[0][2], csvtab[0][3]);
+    fprintf(fp, "10³", csvtab[1][0], csvtab[1][1], csvtab[1][2], csvtab[1][3]);
+    fprintf(fp, "10⁴", csvtab[2][0], csvtab[2][1], csvtab[2][2], csvtab[2][3]);
+    fprintf(fp, "10⁵", csvtab[3][0], csvtab[3][1], csvtab[3][2], csvtab[3][3]);
+    fprintf(fp, "10⁶", csvtab[4][0], csvtab[4][1], csvtab[4][2], csvtab[4][3]);
+    fprintf(fp, "10⁷", csvtab[5][0], csvtab[5][1], csvtab[5][2], csvtab[5][3]);
 
-	fclose(fp);
-	printf("Fichier %s modifié !", filename);
+    fclose(fp);
+    printf("Fichier %s modifié !", filename);
 }
 
 int main()
 {
 
-	//Initialisation d'un tableau permettant de récupérer les valeurs du CSV
-	float csvtab[5][7];
+    //Initialisation d'un tableau permettant de récupérer les valeurs du CSV
+    float csvtab[5][7];
 
-	//Initialisation de nos variables de temps en millisecondes
-	clock_t debut, fin;
+    //Initialisation de nos variables de temps en millisecondes
+    clock_t debut, fin;
 
-	//Initialisation des variable de temps moyen pour les differents tris et des variables permetant de stocker le temps pour chaque test
-	float temp_bulle[5], temp_selec[5], temp_insert[5], temp_tas[5], temp_total[2];
+    //Initialisation des variable de temps moyen pour les differents tris et des variables permetant de stocker le temps pour chaque test
+    float temp_bulle[5], temp_selec[5], temp_insert[5], temp_tas[5], temp_total[2];
 
-	//Initialisation des différentes taille possible pour les tableaux
-	int taille[6] = {100, 1000, 10000, 100000, 1000000, 10000000};
+    //Initialisation des différentes taille possible pour les tableaux
+    int taille[6] = {100, 1000, 10000, 100000, 1000000, 10000000};
 
-	//Initialisation d'une seed random pour la génération des valeurs de nos tableaux
-	srand((unsigned int)time(NULL));
+    //Initialisation d'une seed random pour la génération des valeurs de nos tableaux
+    srand((unsigned int)time(NULL));
 
-	printf("*******************\n");
-	printf("Début du programme.\n");
-	printf("*******************\n\n");
-	
-	for (int f = 0; f < 6; f++)
-	{
-		//Initialisation de tabtri et de tab_global contenant les trois tableaux de valeurs aléatoires
-		float tabtri[taille[f]];
-		float tab_global[3][taille[f]];
+    printf("*******************\n");
+    printf("Début du programme.\n");
+    printf("*******************\n\n");
 
-		//Création des trois listes de valeur aleatoire dans tab_global
-		for (int i = 0; i < taille[f]; i++)
-		{
-			int nb = rand() % 1000000 + 1;
-			tab_global[0][i] = ((float)rand() / (float)(RAND_MAX)) + nb;
-		}
-		for (int j = 0; j < taille[f]; j++)
-		{
-			int nb = rand() % 1000000 + 1;
-			tab_global[1][j] = ((float)rand() / (float)(RAND_MAX)) + nb;
-		}
-		for (int k = 0; k < taille[f]; k++)
-		{
-			int nb = rand() % 1000000 + 1;
-			tab_global[2][k] = ((float)rand() / (float)(RAND_MAX)) + nb;
-		}
+    for (int f = 0; f < 6; f++)
+    {
+        //Initialisation de tabtri et de tab_global contenant les trois tableaux de valeurs aléatoires
+        float tabtri[taille[f]];
+        float tab_global[3][taille[f]];
 
-		//Test de l'algorithme de tri à bulle avec les 3 tableaux différents
-		for (int l = 0; l < 3; l++)
-		{
-			//Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
-			for (int m = 0; m < taille[m]; m++)
-			{
-				tabtri[m] = tab_global[l][m];
-			}
+        //Création des trois listes de valeur aleatoire dans tab_global
+        for (int i = 0; i < taille[f]; i++)
+        {
+            int nb = rand() % 1000000 + 1;
+            tab_global[0][i] = ((float)rand() / (float)(RAND_MAX)) + nb;
+        }
+        for (int j = 0; j < taille[f]; j++)
+        {
+            int nb = rand() % 1000000 + 1;
+            tab_global[1][j] = ((float)rand() / (float)(RAND_MAX)) + nb;
+        }
+        for (int k = 0; k < taille[f]; k++)
+        {
+            int nb = rand() % 1000000 + 1;
+            tab_global[2][k] = ((float)rand() / (float)(RAND_MAX)) + nb;
+        }
 
-			//Lancement du compteur de temps
-			debut = clock();
+        //Test de l'algorithme de tri à bulle avec les 3 tableaux différents
+        for (int l = 0; l < 3; l++)
+        {
+            //Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
+            for (int m = 0; m < taille[f]; m++)
+            {
+                tabtri[m] = tab_global[l][m];
+            }
 
-			//Le tableau se fait trier
-			tri_bulle(tabtri, taille);
+            //Lancement du compteur de temps
+            debut = clock();
 
-			//Arrêt du compteur de temps
-			fin = clock();
+            //Le tableau se fait trier
+            tri_bulle(tabtri, taille[f]);
 
-			//Calcul et affichage du temps de tri du tableau en milliseconde
-			temp_total[l] = fin - debut;
-			printf("Test %d : %.2f ms.\n", l+1, temp_total[l]);
-		}
-		//Calcul et affichage de la moyenne du temps d'exécution du tri à bulle en milliseconde suivant les 3 tris effectués
-		temp_bulle[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
-		printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri à bulle est de : %.2f ms.\n\n", taille[f], temp_bulle);
+            //Arrêt du compteur de temps
+            fin = clock();
 
-		//Test de l'algorithme de tri par sélection avec les 3 tableaux différents
-		for (int l = 0; l < 3; l++)
-		{
-			//Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
-			for (int m = 0; m < taille[m]; m++)
-			{
-				tabtri[m] = tab_global[l][m];
-			}
+            //Calcul et affichage du temps de tri du tableau en milliseconde
+            temp_total[l] = fin - debut;
+            printf("Test %d : %.2f ms.\n", l + 1, temp_total[l]);
+        }
+        //Calcul et affichage de la moyenne du temps d'exécution du tri à bulle en milliseconde suivant les 3 tris effectués
+        temp_bulle[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
+        printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri à bulle est de : %.2f ms.\n\n", taille[f], temp_bulle);
 
-			//Lancement du compteur de temps
-			debut = clock();
+        //Test de l'algorithme de tri par sélection avec les 3 tableaux différents
+        for (int l = 0; l < 3; l++)
+        {
+            //Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
+            for (int m = 0; m < taille[f]; m++)
+            {
+                tabtri[m] = tab_global[l][m];
+            }
 
-			//Le tableau se fait trier
-			tri_insertion(tabtri, taille);
+            //Lancement du compteur de temps
+            debut = clock();
 
-			//Arrêt du compteur de temps
-			fin = clock();
+            //Le tableau se fait trier
+            tri_insertion(tabtri, taille[f]);
 
-			//Calcul et affichage du temps de tri du tableau en milliseconde
-			temp_total[l] = fin - debut;
-			printf("Test %d : %.2f ms.\n", l+1, temp_total[l]);
-		}
-		//Calcul et affichage de la moyenne du temps d'exécution du tri par sélection en milliseconde suivant les 3 tris effectués
-		temp_selec[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
-		printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par sélection est de : %.2f ms.\n\n", taille[f], temp_selec);
+            //Arrêt du compteur de temps
+            fin = clock();
 
-		//Test de l'algorithme de tri par insertion avec les 3 tableaux différents
-		for (int l = 0; l < 3; l++)
-		{
-			//Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
-			for (int m = 0; m < taille[m]; m++)
-			{
-				tabtri[m] = tab_global[l][m];
-			}
+            //Calcul et affichage du temps de tri du tableau en milliseconde
+            temp_total[l] = fin - debut;
+            printf("Test %d : %.2f ms.\n", l + 1, temp_total[l]);
+        }
+        //Calcul et affichage de la moyenne du temps d'exécution du tri par sélection en milliseconde suivant les 3 tris effectués
+        temp_selec[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
+        printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par sélection est de : %.2f ms.\n\n", taille[f], temp_selec);
 
-			//Lancement du compteur de temps
-			debut = clock();
+        //Test de l'algorithme de tri par insertion avec les 3 tableaux différents
+        for (int l = 0; l < 3; l++)
+        {
+            //Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
+            for (int m = 0; m < taille[f]; m++)
+            {
+                tabtri[m] = tab_global[l][m];
+            }
 
-			//Le tableau se fait trier
-			tri_insertion(tabtri, taille);
+            //Lancement du compteur de temps
+            debut = clock();
 
-			//Arrêt du compteur de temps
-			fin = clock();
+            //Le tableau se fait trier
+            tri_insertion(tabtri, taille[f]);
 
-			//Calcul et affichage du temps de tri du tableau en milliseconde
-			temp_total[l] = fin - debut;
-			printf("Test %d : %.2f ms.\n", l+1, temp_total[l]);
-		}
-		//Calcul et affichage de la moyenne du temps d'exécution du tri par insertion en milliseconde suivant les 3 tris effectués
-		temp_insert[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
-		printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par insertion est de : %.2f ms.\n\n", taille[f], temp_insert);
+            //Arrêt du compteur de temps
+            fin = clock();
 
-		//Test de l'algorithme de tri par tas avec les 3 tableaux différents
-		for (int l = 0; l < 3; l++)
-		{
-			//Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
-			for (int m = 0; m < taille[m]; m++)
-			{
-				tabtri[m] = tab_global[l][m];
-			}
+            //Calcul et affichage du temps de tri du tableau en milliseconde
+            temp_total[l] = fin - debut;
+            printf("Test %d : %.2f ms.\n", l + 1, temp_total[l]);
+        }
+        //Calcul et affichage de la moyenne du temps d'exécution du tri par insertion en milliseconde suivant les 3 tris effectués
+        temp_insert[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
+        printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par insertion est de : %.2f ms.\n\n", taille[f], temp_insert);
 
-			//Lancement du compteur de temps
-			debut = clock();
+        //Test de l'algorithme de tri par tas avec les 3 tableaux différents
+        for (int l = 0; l < 3; l++)
+        {
+            //Transfere du l-eme tableau dans tabtri (c'est à dire le tableau qui va être trié)
+            for (int m = 0; m < taille[f]; m++)
+            {
+                tabtri[m] = tab_global[l][m];
+            }
 
-			//Le tableau se fait trier
-			tri_tas(tabtri, taille);
+            //Lancement du compteur de temps
+            debut = clock();
 
-			//Arrêt du compteur de temps
-			fin = clock();
+            //Le tableau se fait trier
+            tri_tas(tabtri, taille[f]);
 
-			//Calcul et affichage du temps de tri du tableau en milliseconde
-			temp_total[l] = fin - debut;
-			printf("Test %d : %.2f ms.\n", l+1, temp_total[l]);
-		}
-		//Calcul et affichage de la moyenne du temps d'exécution du tri par tas en milliseconde suivant les 3 tris effectués
-		temp_tas[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
-		printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par tas est de : %.2f ms.\n\n", taille[f], temp_tas);
-	}
+            //Arrêt du compteur de temps
+            fin = clock();
 
-	/* create_csv(?,csvtab); */
-	printf("*******************\n");
-	printf("Programme terminé.\n");
-	printf("*******************\n");
+            //Calcul et affichage du temps de tri du tableau en milliseconde
+            temp_total[l] = fin - debut;
+            printf("Test %d : %.2f ms.\n", l + 1, temp_total[l]);
+        }
+        //Calcul et affichage de la moyenne du temps d'exécution du tri par tas en milliseconde suivant les 3 tris effectués
+        temp_tas[f] = (temp_total[0] + temp_total[1] + temp_total[2]) / 3;
+        printf("La moyenne de temps pour effectuer les tests d'un tableau de %d valeurs pour un tri par tas est de : %.2f ms.\n\n", taille[f], temp_tas);
+    }
+
+    /* create_csv(?,csvtab); */
+    printf("*******************\n");
+    printf("Programme terminé.\n");
+    printf("*******************\n");
 }
